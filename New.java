8ab773f86527638c3c078124809bf97000b10f3c
Sahil Bhosale <134532505+Sahil110102@users.noreply.github.com>
2024-04-28 15:26:45 +0530

    First commit

--- New.java
+++ New.java
@@ -1,5 +1,7 @@
 public class New {
     public static void main(String[] args) {
         System.out.println("Iam a new file");
+        System.out.println("New jav afile crated ");
+        System.out.println("this is my new file ");
     }
 }
